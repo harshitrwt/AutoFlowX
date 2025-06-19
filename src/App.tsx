80759c5d2da959cb761646ab5de702d4c7d92c8b
itsharshitrwt <harshit.rawat1804@gmail.com>
2025-06-20 01:12:51 +0530

    added-analytics

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Index from "./pages/Index";
 import Generator from "./pages/Generator";
 import Examples from "./pages/Examples";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
@@ -27,6 +28,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <Analytics />
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
